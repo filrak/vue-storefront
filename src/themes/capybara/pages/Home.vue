7070f35d13eded8f693f44bd93cdca0d73198b60
Filip Rakowski <frakowski@divante.pl>
2019-01-24 19:33:48 +0100

    Banner componnet basics

--- src/themes/capybara/pages/Home.vue
+++ src/themes/capybara/pages/Home.vue
@@ -1,10 +1,25 @@
 <template>
   <div id="home">
-    <div class="top-category-nav margin-small margin-big">
-      <vsf-button full-width color="secondary" class="margin-small">Women</vsf-button>
-      <vsf-button full-width color="secondary" class="margin-small">Men</vsf-button>
+    <div class="top-category-nav margin-big">
+      <vsf-button full-width color="secondary" class="margin-right-small">Women</vsf-button>
+      <vsf-button full-width color="secondary" class="margin-left-small">Men</vsf-button>
     </div>
     <popular-categories />
+    <vsf-banner class="margin-big" width="100%">
+      <template slot="subtitle">Subtitle</template>
+      <template slot="title">Title</template>
+      <template slot="description">Description</template>
+    </vsf-banner>
+    <vsf-banner class="margin-big" width="50%">
+      <template slot="subtitle">Subtitle</template>
+      <template slot="title">Title</template>
+      <template slot="description">Description</template>
+    </vsf-banner>
+    <vsf-banner class="margin-big" width="50%">
+      <template slot="subtitle">Subtitle</template>
+      <template slot="title">Title</template>
+      <template slot="description">Description</template>
+    </vsf-banner>
     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
