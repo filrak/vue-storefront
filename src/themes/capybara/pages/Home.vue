be1ab393b5ad46e84761b9a5d0582af98759f99e
Filip Rakowski <frakowski@divante.pl>
2019-01-29 10:30:28 +0100

    added newsletter banner

--- src/themes/capybara/pages/Home.vue
+++ src/themes/capybara/pages/Home.vue
@@ -1,25 +1,35 @@
 <template>
   <div id="home">
     <div class="top-category-nav margin-big">
-      <vsf-button full-width color="secondary" class="margin-right-small">Women</vsf-button>
-      <vsf-button full-width color="secondary" class="margin-left-small">Men</vsf-button>
+      <vsf-button full-width color="secondary" class="margin-right-small" text="women" />
+      <vsf-button full-width color="secondary" class="margin-left-small" text="men" />
     </div>
     <popular-categories />
-    <vsf-banner class="margin-big" width="100%">
+    <vsf-banner class="margin-x-big" width="100%">
       <template slot="subtitle">Subtitle</template>
       <template slot="title">Title</template>
       <template slot="description">Description</template>
     </vsf-banner>
-    <vsf-banner class="margin-big" width="50%">
-      <template slot="subtitle">Subtitle</template>
-      <template slot="title">Title</template>
-      <template slot="description">Description</template>
-    </vsf-banner>
-    <vsf-banner class="margin-big" width="50%">
-      <template slot="subtitle">Subtitle</template>
-      <template slot="title">Title</template>
-      <template slot="description">Description</template>
-    </vsf-banner>
+    <div class="half-width-banners margin-big">
+      <vsf-banner class="margin-right-small" width="50%">
+        <template slot="subtitle">Subtitle</template>
+        <template slot="title">Title</template>
+        <template slot="description">Description</template>
+      </vsf-banner>
+      <vsf-banner class="margin-left-small" width="50%">
+        <template slot="subtitle">Subtitle</template>
+        <template slot="title">Title</template>
+        <template slot="description">Description</template>
+      </vsf-banner>
+    </div>
+    <vsf-call-to-action
+      message="Subscribe to Newsletter"
+      button-text="susbcribe"
+      class="cta-newsletter margin-y-big"
+    />
+    <section>
+      <h1>Share your look</h1>
+    </section>
     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -36,6 +46,7 @@
 <script>
 import VsfButton from 'storefront-ui/src/components/Button/Button.vue'
 import VsfBanner from 'storefront-ui/src/components/Banner/Banner.vue'
+import VsfCallToAction from 'storefront-ui/src/components/CallToAction/CallToAction.vue'
 import PopularCategories from 'theme/components/PopularCategories'
 
 export default {
@@ -43,6 +54,7 @@ export default {
   components: {
     VsfButton,
     VsfBanner,
+    VsfCallToAction,
     PopularCategories
   },
   computed: {},
@@ -55,5 +67,15 @@ export default {
     display: flex;
     flex-flow: row nowrap;
     justify-content: space-around;
+  }
+  .half-width-banners {
+    display: flex;
+    flex-flow: row nowrap;
+    .vsf-banner {
+      flex-grow: 1;
+    }
+  }
+  .cta-newsletter {
+    color: white;
   }
 </style>
