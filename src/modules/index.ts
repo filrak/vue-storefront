1fe570cffb91510b8e306c09b72a53050f4f1252
patzick <tomczyk.patryk@gmail.com>
2019-02-05 14:11:41 +0100

    add develop merge

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -8,7 +8,6 @@ import { Review } from '@vue-storefront/core/modules/review'
 import { Mailer } from '@vue-storefront/core/modules/mailer'
 import { Wishlist } from '@vue-storefront/core/modules/wishlist'
 import { Mailchimp } from '../modules/mailchimp'
-<<<<<<< HEAD
 // import { Notification } from '@vue-storefront/core/modules/notification'
 // import { RecentlyViewed } from '@vue-storefront/core/modules/recently-viewed'
 // import { Homepage } from "./homepage"
@@ -22,22 +21,6 @@ import { Mailchimp } from '../modules/mailchimp'
 // import { RawOutputExample } from './raw-output-example'
 // import { Magento2CMS } from './magento-2-cms'
 
-=======
-import { Notification } from '@vue-storefront/core/modules/notification'
-import { RecentlyViewed } from '@vue-storefront/core/modules/recently-viewed'
-import { Homepage } from "./homepage"
-import { Claims } from './claims'
-import { PromotedOffers } from './promoted-offers'
-import { Ui } from './ui-store'
-// import { GoogleAnalytics } from './google-analytics';
-// import { Hotjar } from './hotjar';
-import { AmpRenderer } from './amp-renderer';
-import { PaymentBackendMethods } from './payment-backend-methods';
-import { PaymentCashOnDelivery } from './payment-cash-on-delivery';
-import { RawOutputExample } from './raw-output-example'
-import { Magento2CMS } from './magento-2-cms'
-// import { Example } from './module-template'
->>>>>>> develop
 
 // const extendCartVuex = {
 //   actions: {
