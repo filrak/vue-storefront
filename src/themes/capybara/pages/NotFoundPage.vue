55ff2d9616a2aa2926531e160c0808cb71e24544
patzick <tomczyk.patryk@gmail.com>
2019-02-05 10:54:47 +0100

    add page not found component

---
+++ src/themes/capybara/pages/NotFoundPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>PAGE NOT FOUND</h1>
+    <router-link to="/"> Back to homepage </router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  async asyncData ({ store, route }) {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/themes/capybara/router/index.js
+++ src/themes/capybara/router/index.js
@@ -1,8 +1,9 @@
 const Home = () => import(/* webpackChunkName: "vsf-home" */ 'theme/pages/Home.vue')
+const NotFoundPage = () => import(/* webpackChunkName: "vsf-not-found" */ 'theme/pages/NotFoundPage.vue')
 
 let routes = [
   { name: 'home', path: '/', component: Home, alias: '/pwa.html' },
-  { name: 'page-not-found', path: '/page-not-found', component: Home }
+  { name: 'page-not-found', path: '/page-not-found', component: NotFoundPage }
 ]
 
 export default routes
